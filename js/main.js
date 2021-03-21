40661b1a3efd1d70d4a0d9e48f773d878f22c118
Erick Estilloso <jenredkiss@users.noreply.github.com>
2021-03-21 18:24:38 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Showing Menu Toggle button
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+/** Remove Menu Mobile */
+
+
+const navLink = document.querySelectorAll('.nav_link')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/**Scroll section active link */
+
+const section = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+    section.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+           document.querySelector('.nav_menu a[href*=' + sectionId +']').classList.add('active')
+        }
+        else{
+            document.querySelector('.nav_menu a[href*=' + sectionId +']').classList.remove('active')
+        }
+
+    })
+}
